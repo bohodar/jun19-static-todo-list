339a93e70049b3f59be60b8d66e8acd3d421b3db
Bohodar <bodik264478@gmail.com>
2019-06-24 17:51:50 +0300

    Updates

---
+++ precache-manifest.d5bcbbb88df466eb43c56543314ec801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f1107ea6f274422141b2fb8e5e592c",
+    "url": "/jun19-static-todo-list/index.html"
+  },
+  {
+    "revision": "767a267978267d10cbd0",
+    "url": "/jun19-static-todo-list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jun19-static-todo-list/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "767a267978267d10cbd0",
+    "url": "/jun19-static-todo-list/static/js/main.5a9a5e40.chunk.js"
+  },
+  {
+    "revision": "bf47b9b176860980020a",
+    "url": "/jun19-static-todo-list/static/js/runtime~main.f63226f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jun19-static-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
