037307cf69771cc400739c93a5a37ee6e2bd92e1
Bohodar <bodik264478@gmail.com>
2019-06-26 18:57:59 +0300

    Updates

--- precache-manifest.6b098dc4e2c6121c02fa76f653e812d5.js
+++ precache-manifest.6b098dc4e2c6121c02fa76f653e812d5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cffb470eb6528635be00a7c7682a8d04",
+    "revision": "44ba14d4aeb967831f5e7f5459dbddae",
     "url": "/jun19-static-todo-list/index.html"
   },
   {
-    "revision": "df5a930311dda060740b",
-    "url": "/jun19-static-todo-list/static/css/main.aae7ba7f.chunk.css"
+    "revision": "aaa57645d92872132086",
+    "url": "/jun19-static-todo-list/static/css/main.a22aac4c.chunk.css"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/jun19-static-todo-list/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "df5a930311dda060740b",
-    "url": "/jun19-static-todo-list/static/js/main.f9ae0238.chunk.js"
+    "revision": "aaa57645d92872132086",
+    "url": "/jun19-static-todo-list/static/js/main.39dff805.chunk.js"
   },
   {
     "revision": "bf47b9b176860980020a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jun19-static-todo-list/precache-manifest.037141e69423ba56e0ebba8aaf16ca5a.js"
+  "/jun19-static-todo-list/precache-manifest.6b098dc4e2c6121c02fa76f653e812d5.js"
 );
 
 self.addEventListener('message', (event) => {
